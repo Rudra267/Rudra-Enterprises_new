c48b8fbfb51e93a92842179ad507fb8f5e98b36e
user.name:"Rudra267 <nrudra550@gmail.com>
2025-09-27 16:20:12 +0530

    u

--- js/script.js
+++ js/script.js
@@ -28,3 +28,53 @@ document.querySelector('.close').addEventListener('click',(e)=>{
    e.target.classList.add("open");
   e.target.classList.remove("close");
 })
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+// Animate each card image individually
+gsap.utils.toArray(".featured-card").forEach(card => {
+  let imgBox = card.querySelector(".image-wrap");
+
+  gsap.fromTo(imgBox,
+    { height: "0%" },
+    {
+      height: "100%",
+      ease: "power4.out",
+      duration:1.2,
+      scrollTrigger: {
+        trigger: imgBox,
+        start: "top 30%",   // when the image enters viewport
+        end: "top 0%",     // animation finishes halfway into view
+               // scroll-linked animation
+          // set true for debugging
+      }
+    }
+  );
+});
+
+
+
+
+
+let graden_image = gsap.timeline({
+  scrollTrigger:{
+    trigger:".garden",
+    start:"50% 50%",
+    end:"50% 20%",
+    markers: true, 
+    pin:true,
+    scrub:2.8
+  }
+});
+
+graden_image.to('.garden > .box',{
+    height:"0%",
+    duration:4,
+},"none")
+
+graden_image.to('.garden > .box > p',{
+    opacity:0,
+    duration:2,
+},"none")
